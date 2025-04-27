24a302f0dc4211caeab417fe458c13619ffa6093
aalma <aalma@example.com>
2025-04-27 20:22:38 +0300

    create Workout model linked to User

--- fitgoals/models.py
+++ fitgoals/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Workouts(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    date = models.DateField()
+    activity = models.CharField(max_length=100)
+    duration = models.IntegerField()  # in minutes
+    set = models.CharField(max_length=100)
+    reps = models.IntegerField()
+    weight = models.IntegerField()  # in kg
+    notes = models.TextField(blank=True, null=True)
+
+
+    # new code below
+    def __str__(self):
+        return self.name
+
+
+
