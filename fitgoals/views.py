7d799511b1a4b65dfa49bd3b45ee9e6fa05c605e
aalma <aalma@example.com>
2025-04-28 18:23:44 +0300

    styling and bootstrap and some other fixes

--- fitgoals/views.py
+++ fitgoals/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def dashboard(request):
+    return render(request, 'dashboard.html')
